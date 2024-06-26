91c072f00c295d36d8356478d0f70713794dd912
gabofrid <143649861+gabofrid@users.noreply.github.com>
2024-06-26 10:42:45 -0300

    Uptate generador productos aleatorios, falta ver como se llama boton de reseteo

--- Assets/Scripts/PRODUCTSGENERATOR.cs
+++ Assets/Scripts/PRODUCTSGENERATOR.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class PRODUCTSGENERATOR : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject [] productos;
+    int DineroDisponible;
+    int precio1;
+    int precio2;
+
+    List<int> ProductosConPrecios = new List<int>();
+      void Start()
     {
-        
-    }
+        DineroDisponible = Random.Range(100, 1000);
+        PreciosAleatorios();
+     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void PreciosAleatorios()
+    {
+        int index1 = Random.Range(0, productos.Length);     // metodo para agarrar dos objetos randoms entre los productos
+        int index2 = Random.Range(0, productos.Length -1);
+        if (index2 == index1)   // para ver que no agarre el mismo objeto
+        {
+            index2++;
+        }
+        precio1 = Random.Range(100, 1000);
+        precio2 = Random.Range(100, 1000);
+
+
+        ProductosConPrecios.Add (precio1);
+        ProductosConPrecios.Add(precio2);
+
+
+    }
+    private void JugardeNuevo()
+    {
+        // si tocan el boton reiniciar
+        DineroDisponible = Random.Range(100, 1000);
+        ProductosConPrecios.Clear();
+        PreciosAleatorios();
     }
 }
